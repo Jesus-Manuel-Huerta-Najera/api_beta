7016211098da5916bd8d87571bd3a211e815be62
Jesus Huerta <1718110393@utectulancingo.edu.mx>
2020-02-20 10:33:19 -0600

    Put funcional

--- application/controllers/api_universidad.py
+++ application/controllers/api_universidad.py
@@ -11,31 +11,33 @@ class Action:
                 if data['action'] == "get":
                     with open ('static/csv/alumnos.csv','r') as csvfiles:
                         reader =csv.DictReader(csvfiles)
-                        na = []
+                        result=[]
                         caja = []
                         text = {}
                         text ['status'] = "200 ok"
                         text ['app_version'] = "0.0.1"
+                        # text ['Alumno'] = na
                         caja.append(text)
-                        result={}
                         for row in reader:
-                            na.append(result)
-                            result ['matricula'] = str(row['matricula'])
-                            result ['nombre'] =  str(row['nombre'])
-                            result ['primer_apellido'] = str(row['primer_apellido'])
-                            result ['segundo_apellido'] = str(row['segundo_apellido'])
-                            result ['carrera'] = str(row['carrera'])
-                            text ['Alumno'] = na
-                    return json.dumps(caja)
+                            result.append(row)
+                            text ['Alumno'] = result
+                    return json.dumps(text)
                 elif data["action"] == "put":
-                    data["matricula"]
-                    data["nombre"]
-                    data["primer_apellido"]
-                    data["segundo_apellido"]
-                    data["carrera"]
-                    with open ('static/csv/datos.csv','a+', newline = '') as csvfile:
-                        writer = csv.writer(csvfile)
+                    v1 = data["matricula"]
+                    v2 = data["nombre"]
+                    v3 = data["primer_apellido"]
+                    v4 = data["segundo_apellido"]
+                    v5 = data["carrera"]
+                    result = []
+                    result.append(v1)
+                    result.append(v2)
+                    result.append(v3)
+                    result.append(v4)
+                    result.append(v5)
+                    with open ('static/csv/alumnos.csv','a+', newline = '') as csvfiles:
+                        writer = csv.writer(csvfiles)
                         writer.writerow(result)
+                    return("Hecho")
                 else:
                     result={}
                     text= "Comand not found"
